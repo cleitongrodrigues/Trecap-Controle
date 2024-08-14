39b19ff448eb86bfaa38a8e6bfaae9036c2bbe6a
Cleiton Guilherme Rodrigues de Souza <cleiton.souza50@etec.sp.gov.br>
2024-08-14 15:31:02 -0300

    a

--- controllers/Usuario.js
+++ controllers/Usuario.js
@@ -12,7 +12,55 @@ module.exports = {
         } catch (error) {
             return response.status(500).json({
                 sucesso: false,
-                mensagem: 'Erro na requisição',
+                mensagem: 'Erro ao listar usuário',
+                dados: error.mensagem
+            });
+        }
+    },
+
+    async CadastrarUsuario(request, response){
+        try {
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Usuário cadastro com sucesso!',
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro ao cadastrar usuário',
+                dados: error.mensagem
+            });
+        }
+    },
+
+    async EditarUsuario(request, response){
+        try {
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Usuário cadastro com sucesso!',
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro ao cadastrar usuário',
+                dados: error.mensagem
+            });
+        }
+    },
+
+    async ApagarUsuario(request, response){
+        try {
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Usuário deletado com sucesso!',
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro ao apagar usuário',
                 dados: error.mensagem
             });
         }
--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const UsuarioController = require('../controllers/Usuario');
 
 router.get('/Usuarios', UsuarioController.ListarUSuario);
+router.post('/Usuarios', UsuarioController.CadastrarUsuario);
+router.patch('/Usuario', UsuarioController.EditarUsuario);
+router.delete('/Usuario', UsuarioController.ApagarUsuario);
 
 module.exports = router;
