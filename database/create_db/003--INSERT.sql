7aa918ed00c91cf65972d29c64345c09712cadb0
Cleiton Guilherme Rodrigues de Souza <cleiton.souza50@etec.sp.gov.br>
2024-08-14 11:41:26 -0300

    banco pronto

--- database/create_db/003--INSERT.sql
+++ database/create_db/003--INSERT.sql
@@ -10,61 +10,66 @@ VALUES ('Visitante', b'0');
 
 
 -- Usuário
-INSERT INTO usuario (usu_nome, usu_cpf, usu_telefone, usu_email, usu_login, usu_senha, emp_cod, usu_cargo, usu_departamento, usu_tipo, usu_status, usu_img) 
-VALUES ('Usuário Exemplo', '123.456.789-00', '9876543210', 'usuario@exemplo.com', 'loginusuario', 'senha123', 1, 'Cargo Exemplo', 'Departamento Exemplo', 1, 1, 'img_usuario.jpg');
+INSERT INTO Usuario (usu_nome, usu_CPF, tipo_usuario_id) 
+VALUES ('João Silva', '12345678901', 1);
 
--- Curso
-INSERT INTO curso (curso_nome, curso_prof_cod, curso_area, curso_dur, curso_preco, curso_periodo, curso_dt_aquisicao, curso_img) 
-VALUES ('Curso Exemplo', 1, 1, 120, 499.99, 1, '2024-01-01', 'img_curso.jpg');
+INSERT INTO Usuario (usu_nome, usu_CPF, tipo_usuario_id) 
+VALUES ('Maria Oliveira', '23456789012', 2);
 
--- Matricula_Curso
-INSERT INTO matriculacurso (usu_cod, curso_cod, mat_data, concluido, mat_nota_curso) 
-VALUES (1, 1, '2024-01-01 10:00:00', 0, 0);
+INSERT INTO Usuario (usu_nome, usu_CPF, tipo_usuario_id) 
+VALUES ('Carlos Souza', '34567890123', 2);
 
--- Módulo
-INSERT INTO modulo (mod_nome, mod_num, mod_curso) 
-VALUES ('Módulo Exemplo', 1, 1);
+-- Eventos
+INSERT INTO Eventos (evento_nome, evento_data, evento_local, evento_hora_inicio, evento_hora_termino) 
+VALUES ('Lavagem de Mãos', '2024-09-01', 'Auditório A', '2024-09-01 09:00:00', '2024-09-01 12:00:00');
 
--- Aula
-INSERT INTO aula (aula_nome, aula_url, mod_cod, aula_duracao, aula_orientacao) 
-VALUES ('Aula Exemplo', 'http://exemplo.com/aula', 1, 60, 'Orientação Exemplo');
+INSERT INTO Eventos (evento_nome, evento_data, evento_local, evento_hora_inicio, evento_hora_termino) 
+VALUES ('Secagem de Grãos', '2024-09-15', 'Sala B', '2024-09-15 14:00:00', '2024-09-15 17:00:00');
 
--- Atividade
-INSERT INTO atividade (ativ_data, mat_cod) 
-VALUES ('2024-01-01 10:00:00', 1);
+INSERT INTO Eventos (evento_nome, evento_data, evento_local, evento_hora_inicio, evento_hora_termino) 
+VALUES ('Palestra de Segurança', '2024-10-01', 'Sala C', '2024-10-01 10:00:00', '2024-10-01 11:30:00');
 
--- Empresa_Cursos
-INSERT INTO empresa_cursos (emp_cod, cod_curso, data_aquisicao, data_encerramento) 
-VALUES (1, 1, '2024-01-01', '2024-12-31');
 
--- Comentários
-INSERT INTO comentarios (com_texto, aula_cod, mat_cod) 
-VALUES ('Comentário Exemplo', 1, 1);
 
--- Dúvidas
-INSERT INTO duvidas (mat_cod, usu_cod, duv_texto, duv_data, duv_status) 
-VALUES (1, 1, 'Dúvida Exemplo', '2024-01-01 10:00:00', 1);
+-- Turma
+INSERT INTO Turma (turma_descricao) 
+VALUES ('Turma A - Produção');
 
--- Questionário
-INSERT INTO questionario (mod_cod, qtn_orientacoes, qtn_nquestoes) 
-VALUES (1, 'Orientações Exemplo', 10);
+INSERT INTO Turma (turma_descricao) 
+VALUES ('Turma B - Administração');
 
--- Questões
-INSERT INTO questoes (quest_texto, quest_imagem, mod_cod) 
-VALUES ('Questão Exemplo', 'img_questao.jpg', 1);
+INSERT INTO Turma (turma_descricao) 
+VALUES ('Turma C - Mecânica');
 
--- Alternativas
-INSERT INTO alternativas (alt_texto, alt_imagem, alt_correta, quest_cod) 
-VALUES ('Alternativa Exemplo', 'img_alternativa.jpg', 1, 1);
 
--- Respostas
-INSERT INTO respostas (alt_cod, ativ_cod) 
+-- Colaboradores
+INSERT INTO Colaboradores (colaborador_nome, colaborador_CPF, colaborador_endereco, colaborador_biometria) 
+VALUES ('Pedro Lima', '45678901234', 'Rua A, 123', 'biometria1');
+
+INSERT INTO Colaboradores (colaborador_nome, colaborador_CPF, colaborador_endereco, colaborador_biometria) 
+VALUES ('Ana Costa', '56789012345', 'Rua B, 456', 'biometria2');
+
+INSERT INTO Colaboradores (colaborador_nome, colaborador_CPF, colaborador_endereco, colaborador_biometria) 
+VALUES ('Lucas Martins', '67890123456', 'Rua C, 789', 'biometria3');
+
+
+-- EventoTurma
+INSERT INTO EventoTurma (evento_id, turma_id) 
 VALUES (1, 1);
 
--- Material
-INSERT INTO material (aula_cod, mtr_descricao, mtr_link) 
-VALUES (1, 'Material Exemplo', 'http://exemplo.com/material');
+INSERT INTO EventoTurma (evento_id, turma_id) 
+VALUES (2, 2);
 
--- Progresso
-INSERT INTO progresso (mat_cod, aula_cod, prog_data) 
-VALUES (1, 1, '2024-01-01 10:00:00');
+INSERT INTO EventoTurma (evento_id, turma_id) 
+VALUES (3, 3);
+
+
+-- TurmaColaboradores
+INSERT INTO TurmaColaboradores (turma_id, colaborador_id, turma_colaboradores_comparecimento, usu_id, turma_colaboradores_assinatura) 
+VALUES (1, 1, b'1', 1, 'AssinaturaPedro');
+
+INSERT INTO TurmaColaboradores (turma_id, colaborador_id, turma_colaboradores_comparecimento, usu_id, turma_colaboradores_assinatura) 
+VALUES (2, 2, b'0', 2, 'AssinaturaAna');
+
+INSERT INTO TurmaColaboradores (turma_id, colaborador_id, turma_colaboradores_comparecimento, usu_id, turma_colaboradores_assinatura) 
+VALUES (3, 3, b'1', 3, 'AssinaturaLucas');
