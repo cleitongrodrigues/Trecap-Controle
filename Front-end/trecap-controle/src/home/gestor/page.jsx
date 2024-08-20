91e8b10862f351c06380d7e852f7be8f1d92bdcf
Cleiton Guilherme Rodrigues de Souza <cleiton.souza50@etec.sp.gov.br>
2024-08-20 20:39:28 -0300

    criado a home gestor

--- Front-end/trecap-controle/src/home/gestor/page.jsx
+++ Front-end/trecap-controle/src/home/gestor/page.jsx
@@ -1,7 +1,19 @@
+import style from './page.module.css'
+
 export default function Gestor(){
     return(
-        <div>
-            <h1>teste</h1>
+        <div className={style.Container}>
+            <div className={style.ContainerDivs}>
+                <h1 className={style.Titulo}>Bem Vindo Gestor!</h1>
+                <div className={style.Teste}>
+                    <div className={style.ContainerEventos}>Eventos</div>
+                    <div>Histórico</div>
+                </div>
+                <div className={style.Teste}>
+                    <div>Calendário</div>
+                    <div>Setor</div>
+                </div>
+            </div>
         </div>
     )
 }
