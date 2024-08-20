5079b16f74cd9314ba67a913de145ce8c399082c
Cleiton Guilherme Rodrigues de Souza <cleiton.souza50@etec.sp.gov.br>
2024-08-20 19:55:34 -0300

    a

---
+++ Front-end/trecap-controle/src/login/page.jsx
@@ -0,0 +1,11 @@
+import style from '../login/page.module.css'
+
+export default function Login(){
+    return(
+        <div className={style.ContainerGeral}>
+            <div>
+            <h1 className={style.Titulo}>Teste</h1>
+            </div>
+        </div>
+    )
+}
