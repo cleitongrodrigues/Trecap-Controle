731fb39ab823f53dc3f37660a715abf3f6e352d6
Cleiton Guilherme Rodrigues de Souza <159494633+cleitongrodrigues@users.noreply.github.com>
2024-08-26 19:56:54 -0300

    melhorando eventos

--- Front-end/trecap-controle/src/app/eventos/page.jsx
+++ Front-end/trecap-controle/src/app/eventos/page.jsx
@@ -1,65 +1,73 @@
-'use client'
-import style from './page.module.css'
-import { MdWash, MdPsychology, MdEdit, MdSearch, MdCheck, MdTimer } from "react-icons/md";
-import { IconContext} from 'react-icons';
+'use client';  // Certifique-se de que esta linha está no início do arquivo se estiver usando Next.js com o modo "use client"
 
-export default function Evento(){
-    return(
+import style from './page.module.css';
+import { MdWash, MdPsychology, MdEdit, MdSearch, MdCheck, MdTimer } from "react-icons/md";
+import { IconContext } from 'react-icons';
+
+const eventos = [
+    {
+        titulo: 'Treinamento sobre higiene no trabalho',
+        data: 'Data: 20/08/2024',
+        IconeTipo: "Psicologia"
+    },
+
+    {
+        titulo: 'Treinamento ',
+        data: 'Data: 20/08/2024',
+        IconeTipo: "Wash"
+    },
+
+    {
+        titulo: 'Treinamento teste',
+        data: 'Data: 10/08/2025',
+         IconeTipo: "Timer"
+    }
+];
+
+const Icones = {
+    Psicologia(){
+        return(
+            <MdPsychology />
+        )
+    },
+    Wash(){
+        return(
+            <MdWash />
+        )
+    },
+    Timer(){
+        return(
+            <MdTimer />
+        )
+    }
+}
+
+export default function Evento() {
+    return (
         <div className={style.Geral}>
             <div className={style.Container}>
                 <h1 className={style.Titulo}>Eventos</h1>
                 <div className={style.TesteDiv}>
-                <div className={style.ContainerDivs}>
-                <IconContext.Provider value={{size:100}} >
-                            <MdWash />
-                       </IconContext.Provider>
-                   <div className={style.ContainerLabel}>
-                        <label>Treinamento sobre higiene no trabalho</label>
-                        <label>Data: 20/08/2024</label>
-                   </div>
-                   <div className={style.Icones}>
-                   <IconContext.Provider value={{size:45, className:style.ClasseIcone}}>
-                            <MdEdit/>
-                            <MdSearch/>
-                            <MdCheck/>
-                       </IconContext.Provider>
-                   </div>
-                </div>
-                </div>
-                <div className={style.ContainerDivs}>
-                    <IconContext.Provider value={{size:100}}>
-                            <MdPsychology />
-                       </IconContext.Provider>
-                   <div className={style.ContainerLabel}>
-                        <label>Treinamento sobre utilização de EPI´S</label>
-                        <label>Data: 25/12/2024</label>
-                   </div>
-                   <div className={style.Icones}>
-                   <IconContext.Provider value={{size:45, className:style.ClasseIcone}}>
-                            <MdEdit/>
-                            <MdSearch/>
-                            <MdTimer/>
-                       </IconContext.Provider>
-                   </div>
-                </div>
-                <div className={style.ContainerDivs}>
-                    <IconContext.Provider value={{size:100}}>
-                            <MdWash />
-                       </IconContext.Provider>
-                   <div className={style.ContainerLabel}>
-                        <label>Treinamento sobre uniformes</label>
-                        <label>Data: 03/01/2025</label>
-                   </div>
-                   <div className={style.Icones}>
-                        <IconContext.Provider value={{size:45, className:style.ClasseIcone}}>
-                                    <MdEdit/>
-                                    <MdSearch/>
-                                    <MdTimer/>
+                    {eventos.map((evento, index) => (
+                        <div key={index} className={style.ContainerDivs}>
+                            <IconContext.Provider value={{ size: 100 }}>
+                                {Icones[evento.IconeTipo]()}
                             </IconContext.Provider>
-                   </div>
+                            <div className={style.ContainerLabel}>
+                                <label>{evento.titulo}</label>
+                                <label>{evento.data}</label>
+                            </div>
+                            <div className={style.Icones}>
+                                <IconContext.Provider value={{ size: 45 }}>
+                                    <MdEdit />
+                                    <MdSearch />
+                                    <MdTimer />
+                                </IconContext.Provider>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-               
             </div>
         </div>
-    )
+    );
 }
