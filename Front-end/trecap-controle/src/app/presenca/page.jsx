2b2a476cf9f792ebc7a87c662a3a561afec3e9f6
José Edmilson dos Santos Secundo <125932250+josephsecundo@users.noreply.github.com>
2024-08-27 20:39:51 -0300

    cadastro participantes

--- Front-end/trecap-controle/src/app/presenca/page.jsx
+++ Front-end/trecap-controle/src/app/presenca/page.jsx
@@ -7,11 +7,20 @@ export default function CheckinEvento(){
         <CabecalhoLogado />
        
         
-    <div>
+    <div className={styles.Header} >
         
             <div className={styles.checkin}>
                 <h1>Treinamento Sobre Higiene No Trabalho</h1>
-            </div>            
+               
+               <div className={styles.cadastro} >
+               <h2>Lista de Colaboradores</h2>
+               </div>
+
+                <button className={styles.botaoCadastro}>Cadastrar participantes</button>
+               
+            </div>
+
+        
     </div>
         
     </>
