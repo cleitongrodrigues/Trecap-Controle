ba19949e71feffe0ea4d70336ff826dd5a5dfa5f
José Edmilson dos Santos Secundo <125932250+josephsecundo@users.noreply.github.com>
2024-08-27 22:40:22 -0300

    sss

--- Front-end/trecap-controle/src/app/presenca/page.jsx
+++ Front-end/trecap-controle/src/app/presenca/page.jsx
@@ -1,8 +1,19 @@
+"use client";
+
+import { useState } from 'react';
 import styles from '../presenca/page.module.css';
 import CabecalhoLogado from '@/CabecalhoLogado/page';
 
 export default function CheckinEvento(){
-    return(
+    const [mostrarAlerta, setMostrarAlerta] = useState(true);
+
+    const fecharAlerta = () => {
+        setMostrarAlerta(false);
+      };
+
+
+    return(  
+        
         <>
         <CabecalhoLogado />
        
@@ -10,18 +21,25 @@ export default function CheckinEvento(){
     <div className={styles.Header} >
         
             <div className={styles.checkin}>
-                <h1>Treinamento Sobre Higiene No Trabalho</h1>
+                <h1>TREINAMENTO SOBRE HIGIENE NO TRABALHO</h1>
                
+                
                <div className={styles.cadastro} >
                <h2>Lista de Colaboradores</h2>
+                    <div>
+                {mostrarAlerta && (
+                    <div className={styles.alerta}>
+                    <p>Evento sem participantes. Cadastre participantes ao evento.</p>
+                    <button onClick={fecharAlerta} className={styles.botaoFechar}>Fechar</button>
+                    </div>
+      )}
+                    
+                    </div>
                </div>
+               
 
                 <button className={styles.botaoCadastro}>Cadastrar participantes</button>
                
-            </div>
-
-            <div>
-                
             </div>
 
         
