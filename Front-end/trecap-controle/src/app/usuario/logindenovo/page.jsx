932abb3a7093efce8c92ee88b447bcb2bd678525
Douglas-Felipe-Mariano <159494725+Douglas-Felipe-Mariano@users.noreply.github.com>
2024-08-27 22:17:48 -0300

    fazendo firula na tela de login (Doga)

--- Front-end/trecap-controle/src/app/usuario/logindenovo/page.jsx
+++ Front-end/trecap-controle/src/app/usuario/logindenovo/page.jsx
@@ -1,51 +1,66 @@
-import { Poppins } from "next/font/google";
-import styles from './page.module.css'
+"use client";
+import styles from "./page.module.css";
 import Image from "next/image";
 import logoBranca from "../../../assets/logoBranca.svg";
-
-
-const poppins = Poppins({
-    weight: ['400', '700'],
-    subsets: ['latin']
-});
+import { useState } from "react";
+import { IoEyeOffOutline } from "react-icons/io5";
+import { IoEye } from "react-icons/io5";
 
 
 export default function esqueceuSenha() {
-    return (
-        <div className={styles.container}>
-            <div className={styles.divflexbox}>
-                <div className={styles.containerRoxo}>
-                    <div className={styles.row}>
-                        <h1 className={styles.trecapTitlle}>
-                            TreCap
-                        </h1>
-                        <Image
-                        className={styles.image}
-                            src={logoBranca}
-                            width={83}
-                            height={85}
-                            alt="Logo"
-                        />
-                    </div>
-                    <h1 className={styles.trecapSubTitlle}>Treinamento e capacitação para colaboradores</h1>
-                </div>
-                <div className={styles.containerClaro}>
+  const [ocult, setOcult] = useState(<IoEyeOffOutline className={styles.icon}/>);
+  const [keytype, setKeytype] = useState('password')
 
-                    <h3 className={styles.campos}>Email:</h3>
-                    <input className={styles.textBox}
-                        placeholder="Digiete Seu Email"
-                    />
-                    <h3 className={styles.campos}>Senha:</h3>
-                    <input className={styles.textBox}
-                    type='password'
-                        placeholder="Digiete Sua senha"
-                    />
-
-                    <button className={styles.button}>ENVIAR!</button>
-
-                </div>
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.divflexbox}>
+        <div className={styles.containerRoxo}>
+          <div className={styles.row}>
+            <h1 className={styles.trecapTitlle}>TreCap</h1>
+            <Image
+              className={styles.image}
+              src={logoBranca}
+              width={83}
+              height={85}
+              alt="Logo"
+            />
+          </div>
+          <h1 className={styles.trecapSubTitlle}>
+            Treinamento e capacitação para colaboradores
+          </h1>
         </div>
+        <div className={styles.containerClaro}>
+          <div className={styles.gridcontainer}>
+            <h1 className={styles.login}>Login</h1>
+          </div>
+          <div>
+            <h3 className={styles.campos}>Email:</h3>
+            <input className={styles.textBox} placeholder="Digiete Seu Email" />
+          </div>
 
-    )
+          <div>
+            <h3 className={styles.campos}>Senha:</h3>
+            <div className={styles.containerBotao}>
+              <input
+                className={styles.textBox}
+                type={keytype}
+                placeholder="Digiete Sua senha"
+              />
+
+              <button 
+                onClick={() => setOcult( ocult=<IoEyeOffOutline/> ? setOcult(<IoEye/>) : setOcult(<IoEyeOffOutline/>)}
+                className={styles.botao}  
+              >
+                {ocult}
+              </button>
+            </div>
+          </div>
+
+          <div className={styles.gridcontainer}>
+            <button className={styles.button}>ENVIAR!</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
