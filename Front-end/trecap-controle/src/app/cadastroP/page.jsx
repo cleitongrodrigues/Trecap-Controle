7131673df94c1fabd0c5d5f9adad088a3915f802
Gabriel Bezerra De Sousa <gabriel.sousa234@etec.sp.gov.br>
2024-08-30 20:36:57 -0300

    30/08

--- Front-end/trecap-controle/src/app/cadastroP/page.jsx
+++ Front-end/trecap-controle/src/app/cadastroP/page.jsx
@@ -1,7 +1,10 @@
 import style from './page.module.css'
 export default function CadastroP () {
     return(
-     <div className={style.geral}>
+     <div className={style.container}>
+        <h1 className={style.Titulo}>Cadastro de Participantes</h1> 
+
+
 
 
      </div>   
