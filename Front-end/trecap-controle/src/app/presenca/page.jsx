afb385ab8e853d57eaae49d6979abd720f15eee6
José Edmilson dos Santos Secundo <125932250+josephsecundo@users.noreply.github.com>
2024-09-04 21:37:06 -0300

    ss

--- Front-end/trecap-controle/src/app/presenca/page.jsx
+++ Front-end/trecap-controle/src/app/presenca/page.jsx
@@ -5,16 +5,25 @@ import styles from "../presenca/page.module.css";
 import CabecalhoLogado from "@/CabecalhoLogado/page";
 
 export default function CheckinEvento() {
+<<<<<<< Updated upstream
   const [mostrarAlerta, setMostrarAlerta] = useState(true);
 
   const fecharAlerta = () => {
     setMostrarAlerta(false);
   };
+=======
+    const [mostrarAlerta, setMostrarAlerta] = useState(true);
+
+    const fecharAlerta = () => {
+        setMostrarAlerta(false);
+    };
+>>>>>>> Stashed changes
 
   return (
     <>
       <CabecalhoLogado />
 
+<<<<<<< Updated upstream
       <div className={styles.Header}>
         <div className={styles.checkin}>
           <h1>TREINAMENTO SOBRE HIGIENE NO TRABALHO</h1>
@@ -32,9 +41,43 @@ export default function CheckinEvento() {
                   </button>
                 </div>
               )}
+=======
+    return (
+
+        <>
+            <CabecalhoLogado />
+
+
+            <div className={styles.Header} >
+
+                <div className={styles.checkin}>
+                    <h1>TREINAMENTO SOBRE HIGIENE NO TRABALHO</h1>
+
+
+                    <div className={styles.cadastro} >
+                        <h2>Lista de Colaboradores</h2>
+                        <div>
+                            {mostrarAlerta && (
+                                <div className={styles.alerta}>
+                                    <p>Evento sem participantes. Cadastre participantes ao evento.</p>
+                                    <button onClick={fecharAlerta} className={styles.botaoFechar}>Fechar</button>
+                                </div>
+                            )}
+
+                        </div>
+                    </div>
+
+
+                    <button className={styles.botaoCadastro}>Cadastrar participantes</button>
+
+                </div>
+
+
+>>>>>>> Stashed changes
             </div>
           </div>
 
+<<<<<<< Updated upstream
           <button className={styles.botaoCadastro}>
             Adicionar participantes
           </button>
@@ -43,3 +86,10 @@ export default function CheckinEvento() {
     </>
   );
 }
+=======
+        </>
+
+
+    )
+}
+>>>>>>> Stashed changes
