9875e8812041089fb92e30db0243a0a2ca785778
Gabriel Bezerra De Sousa <gabriel.sousa234@etec.sp.gov.br>
2024-09-04 22:46:49 -0300

    04/09

--- Front-end/trecap-controle/src/app/cadastroP/page.jsx
+++ Front-end/trecap-controle/src/app/cadastroP/page.jsx
@@ -1,9 +1,10 @@
 "use client";
 import styles from './page.module.css'
-import {useState} from 'react'
+import { useState } from 'react'
+import CabecalhoLogado from '@/cabecalhoLogado/page';
 
-export default function CadastroP () {
-    
+export default function CadastroP() {
+
   const [selectedSetores, selectedSetoresSetores] = useState({
     producao: true,
     cargaDescarga: true,
@@ -11,12 +12,12 @@ export default function CadastroP () {
     almoxarifado: true,
 
   });
-  
+
   const handleCheckboxChange = (e) => {
-    const {name, checked } = e.target;
+    const { name, checked } = e.target;
     selectedSetoresSetores((prev) => ({
-     ...prev,
-     [name]: checked,       
+      ...prev,
+      [name]: checked,
 
     }));
   };
@@ -24,59 +25,63 @@ export default function CadastroP () {
   const handleLIstar = () => {
     console.log('Setores selecionados:', selectedSetores);
   };
-  
-  return(
-    <div className={styles.container}>
+
+  return (
+    <>
+      <CabecalhoLogado />
+      <div className={styles.container}>
         <div className={styles.header}>
-            <h1>Treinamento sobre Higiene no Trabalho</h1>
+          <h1>Treinamento sobre Higiene no Trabalho</h1>
         </div>
         <div className={styles.content}>
-            <h2>Cadastro de Participantes</h2>
-            <div className={styles.setores}>
-              <div className={styles.set} >
-                  <h3>Selecione setores participantes</h3>
-              </div>
-                <div className={styles.checkbox}>
-                    <label>
-                        <input
-                        type="checkbox"
-                        name="producao"
-                        checked={selectedSetores.producao}
-                        onChange={handleCheckboxChange}
-                        />
-                        Produção
-                    </label>
-                    <label>
-                        <input
-                        type='checkbox'
-                        name='cargaDescarga'
-                        checked={selectedSetores.cargaDescarga}
-                        onChange={handleCheckboxChange}
-                        />
-                        Carga e Descarga
-                    </label>
-                    <label>
-                        <input
-                        type='checkbox'
-                        name='almoxarifado'
-                        checked={selectedSetores.almoxarifado}
-                        onChange={handleCheckboxChange}
-                        />
-                        Almoxarifado
-                    </label>
-                </div>
+          <h2>Cadastro de Participantes</h2>
+          <div className={styles.content2}>
+          <div className={styles.setores}>
+            <div className={styles.set} >
+              <h3>Selecione setores participantes</h3>
             </div>
+            <div className={styles.checkbox}>
+              <label>
+                <input
+                  type="checkbox"
+                  name="producao"
+                  checked={selectedSetores.producao}
+                  onChange={handleCheckboxChange}
+                />
+                Produção
+              </label>
+              <label>
+                <input
+                  type='checkbox'
+                  name='cargaDescarga'
+                  checked={selectedSetores.cargaDescarga}
+                  onChange={handleCheckboxChange}
+                />
+                Carga e Descarga
+              </label>
+              <label>
+                <input
+                  type='checkbox'
+                  name='almoxarifado'
+                  checked={selectedSetores.almoxarifado}
+                  onChange={handleCheckboxChange}
+                />
+                Almoxarifado
+              </label>
+            </div>
+          </div>
+          </div>
         </div>
-        <button className={styles.button}onClick={handleLIstar}>Listar</button> 
-    </div> 
-
+        <button className={styles.button} onClick={handleLIstar}>Listar</button>
+      </div>
+    </>
   )
 
 }
-    
-    
-    
-    
-    
- 
+
+
+
+
+
+
 
