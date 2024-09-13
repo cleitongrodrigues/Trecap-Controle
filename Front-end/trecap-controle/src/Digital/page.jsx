7064d6ac9c725ba079f1575b2075823fb87775fb
Gabriel Bezerra De Sousa <gabriel.sousa234@etec.sp.gov.br>
2024-09-13 20:34:57 -0300

    13/09

---
+++ Front-end/trecap-controle/src/Digital/page.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import Image from 'next/image'
+import styles from './page.module.css'
+import {useState} from 'react'
+import CabecalhoLogado from '@/cabecalhoLogado/page';
+
+export default function Digital() {
+    return(
+        <div className={styles.container}>
+            
+        </div>
+    ) 
+}
