5f25c3a2b07dadb192bccebd878ae0fb778f368b
Gabriel Bezerra De Sousa <159494662+bezerra26007@users.noreply.github.com>
2024-09-16 15:53:37 -0300

    16/09

--- Front-end/trecap-controle/src/app/Calendario/page.jsx
+++ Front-end/trecap-controle/src/app/Calendario/page.jsx
@@ -2,25 +2,28 @@
 import styles from './page.module.css'
 import { useState } from 'react'
 import CabecalhoLogado from '@/cabecalhoLogado/page';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
-const Mycalendar = () => {
-    const [date, setDate] = useState(new Date());
 
-    <CabecalhoLogado />
-    return (
-        <div className={styles.containerCalendar}>
-            <h2> Calendario Empresarial</h2>
-            <Calendar
-            onChange={setDate}
-            value={date}
-            locale='pt-BR'
-            />
-
-            <p>Data selecionada: {date.toLocaleDateString()}</p>
-        </div>
-    );
+const MyFullCalendar = () => {
+  const events = [
+    { title: 'Reunião de Equipe', date: '2024-09-20' },
+    { title: 'Apresentação ao Cliente', date: '2024-09-22' },
+  ];
+<CabecalhoLogado />
+  return (
+    <div style={{ maxWidth: "80%", margin: "0 auto" }}>
+      <h2>Calendário Empresarial</h2>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin]}
+        initialView="dayGridMonth"
+        events={events}
+       
+      />
+    </div>
+  );
 };
 
-export default Mycalendar
+export default MyFullCalendar;
