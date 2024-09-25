a0e84f3a96bc09cb5197be477dc84e0595209822
Cleiton Guilherme Rodrigues de Souza <159494633+cleitongrodrigues@users.noreply.github.com>
2024-09-25 19:45:40 -0300

    boa

--- Front-end/trecap-controle/src/components/menuLateral/page.jsx
+++ Front-end/trecap-controle/src/components/menuLateral/page.jsx
@@ -1,1 +1,39 @@
-export default function Menu(){}
+// components/MenuLateral.js
+import Link from "next/link";
+import { IconContext } from "react-icons";
+import { MdCalendarMonth, MdHourglassBottom, MdEventNote, MdPeople, MdSettings, MdAccountCircle } from "react-icons/md";
+import styles from './page.module.css'; // CSS para o menu lateral
+
+const MenuLateral = () => {
+  return (
+    <div className={styles.menuLateral}>
+      <IconContext.Provider value={{ size: "2rem" }}>
+        <div className={styles.menuItems}>
+          <Link href="/eventos">
+            <MdEventNote /> Eventos
+          </Link>
+          <Link href="/historico">
+            <MdHourglassBottom /> Histórico
+          </Link>
+          <Link href="/calendario">
+            <MdCalendarMonth /> Calendário
+          </Link>
+          <Link href="/colaboradores">
+            <MdPeople /> Colaboradores
+          </Link>
+        </div>
+
+        <div className={styles.menuFooter}>
+          <Link href="/configuracoes">
+            <MdSettings /> Configurações
+          </Link>
+          <Link href="/perfil">
+            <MdAccountCircle /> Perfil
+          </Link>
+        </div>
+      </IconContext.Provider>
+    </div>
+  );
+};
+
+export default MenuLateral;
