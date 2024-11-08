7c7521109e30b05566506c6d4646dd68d56ea7bc
Cleiton Guilherme Rodrigues de Souza <159494633+cleitongrodrigues@users.noreply.github.com>
2024-11-08 20:17:07 -0300

    resolver o BO de alterar a imagem

--- Back-end/midleware/uploadimage.js
+++ Back-end/midleware/uploadimage.js
@@ -1,7 +1,10 @@
 const multer = require("multer");
+const path = require("path");
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/images");
+    // cb(null, "./public/images");
+    cb(null, path.join(__dirname, 'public', 'images'));
   },
   filename: function (req, file, cb) {
     //let data = new Date().toISOString().replace(/:/g, '-') + '-';
