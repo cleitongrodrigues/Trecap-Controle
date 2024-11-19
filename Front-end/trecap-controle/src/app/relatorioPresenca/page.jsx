cf5f7bd08ea8ee29c789e233647215d2cc679ef8
José Edmilson dos Santos Secundo <125932250+josephsecundo@users.noreply.github.com>
2024-11-19 01:18:28 -0300

    Alterando Relatorio

--- Front-end/trecap-controle/src/app/relatorioPresenca/page.jsx
+++ Front-end/trecap-controle/src/app/relatorioPresenca/page.jsx
@@ -17,16 +17,19 @@ export default function RelatorioPresenca() {
   const [horarioInicioEvento, setHorarioInicioEvento] = useState("");
 
   useEffect(() => {
-    
-    async function getEventoNome(eventoSelecionado)
-    {
-      const response = await axios.get(`http://localhost:3333/Eventos/${eventoSelecionado}`);
-      const evento = response.data.dados[0];
-      setEventoSelecionado(evento.evento_nome);
+    async function getEventoNome(eventoId) {
+      try {
+        const response = await axios.get(`http://localhost:3333/Eventos/${eventoId}`);
+        const evento = response.data.dados[0];
+        setEventoSelecionado(evento.evento_nome);
+      } catch (error) {
+        console.error("Erro ao buscar nome do evento:", error);
+      }
     }
+ 
 
     const fetchEventoData = async () => {
-      const eventoId = localStorage.getItem('eventoSelecionado');
+      const eventoId = localStorage.getItem("eventoSelecionado");
       try {
         const response = await fetch(`http://localhost:3333/evento/${eventoId}`);
         if (!response.ok) {
@@ -42,36 +45,29 @@ export default function RelatorioPresenca() {
     };
 
     if (typeof window !== "undefined") {
-      // Recuperando os participantes do localStorage e associando nome com horário
       const participantesLocalStorage = JSON.parse(localStorage.getItem("participantesPresentes")) || {};
-      
-      // Transformar o objeto em um array com nome e horário
+
       const participantesArray = Object.entries(participantesLocalStorage).map(([id, { nome, hora }]) => {
-        const dataHora = dayjs(hora, "DD/MM/YYYY HH:mm:ss"); // Ajustando o formato de data/hora
+        const dataHora = dayjs(hora, "DD/MM/YYYY HH:mm:ss");
         return { nome, horario: dataHora };
       });
 
-      // Ordenando os participantes pela data/hora de chegada
       const participantesOrdenados = participantesArray.sort((a, b) => a.horario - b.horario);
       setParticipantesPresentes(participantesOrdenados);
 
-      // Definindo o nome do evento
-      const evento = localStorage.getItem("eventoSelecionado");
-      getEventoNome(evento)
-
-      // Buscando os dados do evento
+      const eventoId = localStorage.getItem("eventoSelecionado");
+      getEventoNome(eventoId);
       fetchEventoData();
+      
 
-      // Recuperando e setando o horário de início do evento
       const horarioInicio = localStorage.getItem("horarioInicioEvento");
       setHorarioInicioEvento(horarioInicio || "Horário de Início Não Encontrado");
     }
   }, []);
 
-  // Função para converter e formatar data/hora para exibição
   const formatarDataHora = (dataHora) => {
     if (!dataHora || !dataHora.isValid()) {
-      return "Data Inválida"; // Retorna uma mensagem padrão se a data for inválida
+      return "Data Inválida";
     }
     return dataHora.format("DD/MM/YYYY HH:mm");
   };
@@ -80,54 +76,58 @@ export default function RelatorioPresenca() {
     window.print();
   };
 
-  const salvarRelatorioPDF = () => {
+  const salvarRelatorioPDF = async (evento) => {
+  try {
+    const response = await axios.get(`http://localhost:3333/registros/${evento.evento_id}`);
+    if (response.status !== 200) {
+      throw new Error("Erro ao buscar participantes");
+    }
+    const eventos  = response.data.dados;
+    const participantes = response.data.dados;
+
     const doc = new jsPDF();
+    doc.setFontSize(18);
+    doc.text("Relatório de Presença", 14, 22);
+    
+    // Certifique-se de que evento.evento_data_inicio e evento.evento_hora estão no formato correto
 
-    const title = "Relatório de Presença";
-    const pageWidth = doc.internal.pageSize.getWidth();
-    const textWidth = doc.getTextWidth(title);
-    const textX = (pageWidth - textWidth) / 2;
+   
 
-    doc.text(title, textX, 10);
+    const dataEventoFormatada = dayjs(eventos.evento_data_inicio).format("DD/MM/YYYY"); // Formato de data
+    const horaEventoFormatada = dayjs(eventos.evento_hora, "HH:mm:ss").format("HH:mm"); // Formato de hora
 
-    const cursoWidth = doc.getTextWidth(curso);
-    const cursoX = (pageWidth - cursoWidth) / 2;
-    doc.text(curso, cursoX, 20);
+    doc.setFontSize(12);
+    doc.text(`Evento: ${evento.evento_nome}`, 14, 32);
+    doc.text(`Data: ${dataEventoFormatada}`, 14, 40);
+    doc.text(`Horário: ${horaEventoFormatada}`, 14, 48);
 
-    const eventoWidth = doc.getTextWidth(eventoSelecionado.toUpperCase());
-    const eventoX = (pageWidth - eventoWidth) / 2;
-    doc.text(eventoSelecionado.toUpperCase(), eventoX, 30);
+    doc.setLineWidth(0.5);
+    doc.line(14, 53, 200, 53);
 
-    const horarioFormatado = formatarDataHora(dayjs(horarioInicioEvento, "YYYY-MM-DD HH:mm:ss"));
-    const inicioWidth = doc.getTextWidth(`Horário de Início: ${horarioFormatado}`);
-    const inicioX = (pageWidth - inicioWidth) / 2;
-    doc.text(`Horário de Início: ${horarioFormatado}`, inicioX, 50);
-
-    doc.autoTable({
-      head: [["Nome", "Horário de Chegada"]], // Definindo o cabeçalho da tabela
-      body: participantesPresentes.map((participante) => [
-        participante.nome.toUpperCase(), // Exibindo o nome do colaborador
-        formatarDataHora(participante.horario),
-      ]),
-      startY: 60,
-      theme: "striped",
-      headStyles: { fillColor: [74, 20, 140] },
-      styles: { halign: "center" },
+    const colunas = ["Nome", "Setor", "Data de Chegada", "Hora de Chegada"];
+    const linhas = participantes.map((p) => {
+      const dataHoraEntrada = new Date(p.registros_hora_entrada);
+      return [
+        p.colaborador_nome,
+        p.setor_nome || "Desconhecido", // Se o setor não for encontrado, coloca "Desconhecido"
+        dataHoraEntrada.toLocaleDateString("pt-BR"),
+        dataHoraEntrada.toLocaleTimeString("pt-BR"),
+      ];
     });
 
-    const agora = new Date();
-    const dataGeracao = `Data e Hora de Geração: ${agora.toLocaleString("pt-BR")}`;
-    const metodoGeracao = "Relatório gerado usando o sistema de gestão de presença. Trecap";
-    doc.setFontSize(8);
+    doc.autoTable({
+      head: [colunas],
+      body: linhas,
+      startY: 60,
+      theme: "grid",
+      margin: { horizontal: 14 },
+    });
 
-    doc.text(dataGeracao, 10, doc.internal.pageSize.height - 20);
-    doc.text(metodoGeracao, 10, doc.internal.pageSize.height - 15);
-
-    const assinatura = "Assinado digitalmente por: Sistema de Gestão de Presença Trecap";
-    doc.text(assinatura, 10, doc.internal.pageSize.height - 10);
-
-    doc.save("relatorio-presenca.pdf");
-  };
+    doc.save(`Relatorio_Presenca_${evento.evento_nome}.pdf`);
+  } catch (error) {
+    console.error("Erro ao gerar PDF:", error);
+  }
+};
 
   return (
     <>
@@ -160,7 +160,17 @@ export default function RelatorioPresenca() {
             Imprimir Relatório
           </button>
 
-          <button className={styles.botaoImprimir} onClick={salvarRelatorioPDF}>
+          <button
+            className={styles.botaoImprimir}
+            onClick={() =>
+              salvarRelatorioPDF({
+                evento_id: localStorage.getItem("eventoSelecionado"),
+                evento_nome: eventoSelecionado,
+                evento_data_inicio: dataEvento,
+                evento_hora: horarioEvento,
+              })
+            }
+          >
             Salvar Relatório
           </button>
         </div>
